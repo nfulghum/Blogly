a29146db12d0f8adeeff82440c571a1183da6ee7
Nick Fulghum <nickfulghum92@gmail.com>
2022-07-18 15:39:36 -0500

    base template

---
+++ models.py
@@ -0,0 +1,33 @@
+from enum import unique
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+
+def connect_db(app):
+    db.app = app
+    db.init_app(app)
+
+# Models below
+
+
+class User(db.Model):
+    __tablename__ = 'users'
+
+    def __repr__(self):
+        u = self
+        return f'<User id={u.id} first_name={u.first_name} last_name={u.last_name} image_url={u.image_url}'
+
+    id = db.Column(db.Integer,
+                   primary_key=True,
+                   autoincrement=True)
+
+    first_name = db.Column(db.String(20),
+                           nullable=False)
+
+    last_name = db.Column(db.String(20),
+                          nullable=False)
+
+    image_url = db.Column(db.Text,
+                          nullable=False,
+                          default="https: // www.freeiconspng.com/uploads/icon-user-blue-symbol-people-person-generic--public-domain--21.png")
